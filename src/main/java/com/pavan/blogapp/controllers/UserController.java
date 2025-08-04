0416265ae7aa00a089716c0dc3b14f75dd5a8f32
sripavanmaddela <sripavanmaddela@gmail.com>
2025-08-04 19:35:46 +0530

    Added Custom API Response for Delete API

--- src/main/java/com/pavan/blogapp/controllers/UserController.java
+++ src/main/java/com/pavan/blogapp/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.pavan.blogapp.controllers;
 
+import com.pavan.blogapp.payloads.ApiResponse;
 import com.pavan.blogapp.payloads.UserDTO;
 import com.pavan.blogapp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,31 +17,35 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
+    // Crate User
     @PostMapping("/")
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
         UserDTO createdUser = this.userService.createUser(userDTO);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
+    // Get All Users in Repo
     @GetMapping("/")
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
+    // Get User by Id
     @GetMapping("/{userId}")
     public ResponseEntity<UserDTO> getUser(@PathVariable("userId") Long userId){
         return new ResponseEntity<UserDTO>(userService.getUserById(userId), HttpStatus.OK);
     }
 
+    // Update User by Id
     @PutMapping("/{userId}")
     public ResponseEntity<UserDTO> updateUser(@PathVariable("userId") Long userId, @RequestBody UserDTO userDTO){
         return new ResponseEntity<UserDTO>(userService.updateUser(userDTO, userId), HttpStatus.OK);
     }
 
+    // Delete User By Id
     @DeleteMapping("/{userId}")
-    public void deleteUser(@PathVariable("userId") Long userId){
-        this.userService.deleteUser(userId);
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Long userId){
+        return new ResponseEntity<>(new ApiResponse("User Deleted Successfully", false), HttpStatus.OK);
     }
 
 }
