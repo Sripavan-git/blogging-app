e4dce1446d3f829d0763e18cfa9095049898d9b9
sripavanmaddela <sripavanmaddela@gmail.com>
2025-08-14 12:07:44 +0530

    added getAll and getPostById APIs and added postId in the DTO to support getById method

--- src/main/java/com/pavan/blogapp/controllers/PostController.java
+++ src/main/java/com/pavan/blogapp/controllers/PostController.java
@@ -19,6 +19,7 @@ public class PostController {
     @Autowired
     private PostRepository postRepository;
 
+    // Create Post API
     @PostMapping("/user/{userId}/category/{categoryId}/posts")
     public ResponseEntity<PostDTO> createPost(@RequestBody PostDTO postDTO, @PathVariable Long userId, @PathVariable Long categoryId){
         PostDTO createdPost = postService.createPost(postDTO, userId, categoryId);
@@ -26,23 +27,31 @@ public class PostController {
     }
 
 
+    // Get All Posts By User API
     @GetMapping("/user/{userId}/posts")
     public ResponseEntity<List<PostDTO>> getPostsByUser(@PathVariable Long userId){
         List<PostDTO> postDTOS = this.postService.getPostsByUser(userId);
         return new ResponseEntity<>(postDTOS, HttpStatus.OK);
     }
 
+    // Get All Posts by Category API
     @GetMapping("/category/{categoryId}/posts")
     public ResponseEntity<List<PostDTO>> getPostsByCategory(@PathVariable Long categoryId){
         List<PostDTO> postDTOS = this.postService.getPostsByCategory(categoryId);
         return new ResponseEntity<>(postDTOS, HttpStatus.OK);
     }
 
-    @GetMapping
+    // Get All Posts
+    @GetMapping("/posts")
     public ResponseEntity<List<PostDTO>> getAllPosts(){
         return new ResponseEntity<>(postService.getAllPosts(), HttpStatus.OK);
     }
 
+    // Get Post by Id
+    @GetMapping("/posts/{postId}")
+    public ResponseEntity<PostDTO> getPostById(@PathVariable Long postId){
+        return new ResponseEntity<>(postService.getPostById(postId), HttpStatus.OK);
+    }
 
 
 }
--- src/main/java/com/pavan/blogapp/payloads/PostDTO.java
+++ src/main/java/com/pavan/blogapp/payloads/PostDTO.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostDTO {
+    private String postId;
     private String title;
     private String content;
     private String imageName;
