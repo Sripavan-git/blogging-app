b60ebcfb6c6bbad524ea5bae3b70cf3383eec977
sripavanmaddela <sripavanmaddela@gmail.com>
2025-08-14 11:50:33 +0530

    changed Post Entity and Dtos related

--- src/main/java/com/pavan/blogapp/modals/Post.java
+++ src/main/java/com/pavan/blogapp/modals/Post.java
@@ -1,10 +1,19 @@
 package com.pavan.blogapp.modals;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
 @Entity
 @Table(name = "post")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
 
     @Id
--- src/main/java/com/pavan/blogapp/payloads/PostDTO.java
+++ src/main/java/com/pavan/blogapp/payloads/PostDTO.java
@@ -1,6 +1,23 @@
 package com.pavan.blogapp.payloads;
 
+import com.pavan.blogapp.modals.Category;
+import com.pavan.blogapp.modals.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostDTO {
     private String title;
     private String content;
+    private String imageName;
+    private Date addedDate;
+    private UserDTO user;
+    private CategoryDTO category;
 }
